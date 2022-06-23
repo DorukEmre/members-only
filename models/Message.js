4fa92dbc384260ac1df5368cac178e55ffb80004
DorukEmre <doruk.emre@outlook.com>
2022-06-23 10:47:06 +0100

    Replaced luxon with date-fns

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,21 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const format = require('date-fns')
 
-const message  = new Schema({
+const Message  = new Schema({
     title: { type: String },
-    timestamp: { type: Date, default: Date.now },
+    author: { type: String },
+    timestamp: { type: Date, default: Date.now() },
     content: { type: String }
 })
-module.exports = mongoose.model('Message', message)
+// 2022-06-14T00:00:00.000+00:00
+// 2022-06-22T12:19:40.666+00:00
+Message
+  .virtual('timestamp_formatted')
+  .get(function () {
+    return this.timestamp ? format(this.timestamp, 'MM/dd/yyyy') : '';
+  });
+
+
+module.exports = mongoose.model('Message', Message)
